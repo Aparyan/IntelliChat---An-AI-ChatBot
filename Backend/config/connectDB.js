cfb41f3c446797ab5fd1d1bdba72e8cee7f188f9
user name <aryan.prakash.ug20@nsut.ac.in>
2024-01-07 15:34:45 +0530

    changes

--- Backend/config/connectDB.js
+++ Backend/config/connectDB.js
@@ -4,7 +4,7 @@ const colors = require('colors')
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL)
-        console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white)
+        console.log(`DB Connected On ${mongoose.connection.host}`.bgCyan.white)
     } catch (error) {
         console.log(`${error}`.bgRed)
     }
